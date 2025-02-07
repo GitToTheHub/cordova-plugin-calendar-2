088e18d9d4e505d72d913f2da28c1a54b6d173ff
Manuel Beck <manuelbeck87@outlook.de>
2025-02-07 13:55:06 +0100

    Rename cordova-plugin-calendar to cordova-plugin-calendar-native

--- demo/setup.sh
+++ demo/setup.sh
@@ -1,3 +1,3 @@
 cordova platform add android
 cordova platform add ios
-cordova plugin add https://github.com/GitToTheHub/cordova-plugin-calendar
+cordova plugin add https://github.com/GitToTheHub/cordova-plugin-calendar-native
